edec126ac351a5f9cadfffbb1987d879a4bd8421
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2025-07-03 11:19:05 +0200

    YearSerie : use Echarts

--- src/components/Charts/YearSerie.tsx
+++ src/components/Charts/YearSerie.tsx
@@ -3,11 +3,11 @@
  * Graphique standard pour afficher des données annuelles
  */
 
-import { Bar, BarChart, ComposedChart, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis } from "recharts"
 import { from, op } from "arquero"
 import { useDataset } from "../Dataset/hooks"
-import { useControl } from "../Control/Control"
-import { CategoricalChartProps } from "recharts/types/chart/generateCategoricalChart"
+import { SimpleRecord } from "../../types"
+import { EChartsOption } from "echarts"
+import EChartsReact from "echarts-for-react"
 
 interface IYearSerieProps {
     dataset:string
@@ -17,13 +17,13 @@ interface IYearSerieProps {
     valueKey:string
     categoryKey?:string
     stack?: boolean
-    type?: 'bar' | 'line'
+    type?: 'bar' | 'line' | 'area'
 }
-export const ChartYearSerie:React.FC<IYearSerieProps> = ({dataset:dataset_id, categoryKey, valueKey, yearKey, stack=true, type:chart_type='bar', yearControl='year'}) => {
-
+export const ChartYearSerie:React.FC<IYearSerieProps> = ({dataset:dataset_id, categoryKey, valueKey, yearKey, stack:stack_input, type:chart_type='bar', yearControl='year'}) => {
+    const stack = stack_input || chart_type == 'line' ? false : true ; // Pas de stack par défaut pour le type line
     const dataset = useDataset(dataset_id)
     const data = dataset?.data
-    const current_year = useControl(yearControl) 
+    //const current_year = useControl(yearControl) 
 
     const COLORS = [
         '#00448e', // bleu foncé
@@ -39,64 +39,55 @@ export const ChartYearSerie:React.FC<IYearSerieProps> = ({dataset:dataset_id, ca
 
     const chart_data = categoryKey ? data && from(data).groupby(yearKey, categoryKey) //Somme par année et categorykey
                                             .rollup({[valueKey]:op.sum(valueKey)})
-                                            .groupby(yearKey)
-                                            .pivot(categoryKey,valueKey ).objects()
+                                            .groupby(yearKey).objects()
                                             :
                                         data &&  from(data).groupby(yearKey) //Somme par année seulement
                                         .rollup({[valueKey]:op.sum(valueKey)})
                                         .objects()
 
-
+      console.log(chart_data)
     const distinct_cat:string[] | undefined = categoryKey ? 
         data && (from(data).rollup({cat: op.array_agg_distinct(categoryKey)}).object() as { cat: string[] } ).cat
         :
         [valueKey]
 
-    console.log(distinct_cat)
-    const CustomTick = (props:any) => { //Rechart ne fournis pas le type pour ca ?
-        const { payload, x, y} = props
-        const isCurrentYear = payload.value == current_year;
-        return (
-          <text
-            x={x}
-            y={y + 10}
-            textAnchor="middle"
-            fontWeight={isCurrentYear ? 'bold' : 'normal'}
-          >
-            {payload.value}
-          </text>
-        );
-      };
-
-    const ChartCustomType:React.FC<CategoricalChartProps> = (props) => {
-        // Ajouter area ?
-        if (chart_type == 'bar') {
-            return <BarChart {...props}/>
-        } else if (chart_type == 'line') {
-            return <LineChart {...props}/>
-        } else {
-            return <ComposedChart {...props} /> // Type générique de rechart en fallback
+    const series = distinct_cat ? distinct_cat.map((cat, idx) => (
+        {
+            name:cat,
+            type:chart_type === 'area' ? 'line' : chart_type,
+            data :categoryKey ? chart_data?.filter((row:SimpleRecord) => row[categoryKey] === cat).map((row:SimpleRecord) => ([String(row[yearKey]), row[valueKey] ]))
+                              : chart_data,
+            itemStyle:{
+                color:COLORS[idx % COLORS.length],
+           },
+           stack: stack ? 'total' : undefined,
+           areaStyle : chart_type === 'area' ? {} : undefined
         }
+    )) : {};
+
+    const option:EChartsOption = {
+        series:series,
+        legend: {
+            show:true
+        },
+        tooltip: {
+            trigger: 'axis',
+        },
+        xAxis: [
+            {
+                type: 'time'
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value',
+            }
+        ]
     }
      
     return (
-        <ResponsiveContainer height={324} width='100%' >
-            <ChartCustomType data={chart_data}>
-                <Legend />
-                <Tooltip />
-                <XAxis 
-                    dataKey={yearKey}
-                    tick={current_year ? CustomTick : undefined}
-                />
-                {chart_type == 'bar' && distinct_cat?.map((cat, idx) => 
-                            <Bar key={idx} dataKey={cat} stackId={stack ? 'a' : undefined} fill={COLORS[idx % COLORS.length]} /> 
-                )}
-                {chart_type == 'line' && distinct_cat?.map((cat, idx) => 
-                   <Line key={idx} dataKey={cat} stroke={COLORS[idx % COLORS.length]}  /> 
-                )}
+        <EChartsReact option={option}/>
 
-            </ChartCustomType>
-        </ResponsiveContainer>
     )
 
 
