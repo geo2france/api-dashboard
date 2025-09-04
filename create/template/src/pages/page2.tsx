1645cd76d525811363e938078d441c5ac7ec9068
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2025-09-04 17:00:46 +0200

    base template

---
+++ create/template/src/pages/page2.tsx
@@ -0,0 +1,9 @@
+import  { Dashboard } from "@geo2france/api-dashboard/dsl"
+
+export const MaDeuxiemePage = () => (
+ 
+    <Dashboard name="Deuxieme page de test">
+       <div>Page de test</div>
+    </Dashboard>
+
+)
